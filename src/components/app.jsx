ba128d40851d30475475855327a4ffc158b5ad92
ttasjwi <ttasjwi920@gmail.com>
2022-10-13 23:59:50 +0900

    jsx : `<></>`를 사용하여 형제노드 삽입

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,13 +1,12 @@
 import './app.css';
-import React from 'react';
 
 function App() {
     const name = 'ttasjwi';
     return (
-        <React.Fragment>
+        <>
             <h1>Hello! {name}! :)</h1>
             <h1>No Hello! :(</h1>
-        </React.Fragment>
+        </>
     );
 }
 

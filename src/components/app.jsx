b0b2d55fc302227b7589e4f896c600193b74931d
ttasjwi <ttasjwi920@gmail.com>
2022-10-14 00:23:08 +0900

    jsx : `{...}`을 통해 자바스크립트 코드(비즈니스 로직) 삽입

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -6,6 +6,14 @@ function App() {
         <>
             <h1>Hello! {name}! :)</h1>
             <h1>No Hello! :(</h1>
+            {
+                name && <h1>Hello! {name} :)</h1> // 단축 평가로 오른쪽 값이 return 된다.
+            }
+            {
+                ['🍎', '🍓', '🍌'].map(item => (
+                    <h2>{item}</h2>
+                ))
+            }
         </>
     );
 }

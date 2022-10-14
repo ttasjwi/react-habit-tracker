3888749a2a5c42967cd1f6690c8c6ca69d49cad3
ttasjwi <ttasjwi920@gmail.com>
2022-10-14 11:47:31 +0900

    Habit 컴포넌트 스타일링

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -3,19 +3,19 @@ import React, {Component} from 'react';
 class Habit extends Component {
     render() {
         return (
-            <>
+            <li className="habit">
                 <span className="habit-name">Reading</span>
                 <span className="habit-count">8</span>
-                <button>
+                <button className="habit-button habit-increase">
                     <i className="fa-solid fa-square-plus"></i>
                 </button>
-                <button>
+                <button className="habit-button habit-decrease">
                     <i className="fa-solid fa-square-minus"></i>
                 </button>
-                <button>
+                <button className="habit-button habit-delete">
                     <i className="fa-solid fa-trash"></i>
                 </button>
-            </>
+            </li>
         );
     }
 
